e985e5d8758632ff1e0f716762c75f8740f61111
llllOllOOll <7b37b3@gmail.com>
2023-12-22 19:16:29 -0300

    Layout update

--- app/routes/signup/route.tsx
+++ app/routes/signup/route.tsx
@@ -22,9 +22,39 @@ export async function action({ request }: ActionFunctionArgs) {
 export default function SignUp() {
   const email = useActionData<typeof action>();
   return (
-    <>
-      <div className="flex flex-col min-h-screen justify-center items-center">
-        <h1 className="font-bold text-2xl">Sign Up</h1>
+      <div className="flex flex-col min-h-full justify-center px-6 py-12">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="text-2xl font-bold text-center leading-9 tracking-tight mt-10 text-pink-400">Sign Up</h2>
+        </div>
+
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <form method="POST">
+            <div className="mt-3">
+              <label className="font-bold text-sm" htmlFor="email">Email address</label>
+              <div className="mt-2">
+                <input type="text" id="email" name="email" className="ring-1 ring-inset ring-gray-300 shadow-sm w-full h-8 rounded-sm"/>
+              </div>
+            </div>
+
+            <div className="mt-3">
+              <label className="font-bold text-sm" htmlFor="email">Name</label>
+              <div className="mt-2">
+                <input type="text" id="name" name="name" className="ring-1 ring-inset ring-gray-300 shadow-sm w-full h-8 rounded-sm"/>
+              </div>
+              </div>
+
+              <div className="mt-3">
+              <label className="font-bold text-sm" htmlFor="email">Password</label>
+              <div className="mt-2">
+                <input type="password" id="password" name="password" className="ring-1 ring-inset ring-gray-300 shadow-sm w-full h-8 rounded-sm"/>
+              </div>
+              </div>
+              <div className="mt-7">
+                <button className="bg-pink-400 rounded-sm text-white font-bold w-full h-8 hover:bg-pink-700 text-md">Submit</button>
+              </div>
+          </form>
+        </div>
+        {/* 
         <form method="POST">
           <div>
             <div>{email?.email}</div>
@@ -67,8 +97,7 @@ export default function SignUp() {
           <p>
             Already have an account? <span>Log In</span>
           </p>
-        </div>
+        </div> */}
       </div>
-    </>
   );
 }
