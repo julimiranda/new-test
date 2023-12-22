be1df34080bcedb0d5150b2ff459c4de30d546a2
julia <juliasoulid@gmail.com>
2023-12-22 11:14:34 -0300

    Form validation

--- app/routes/signup/route.tsx
+++ app/routes/signup/route.tsx
@@ -1,11 +1,46 @@
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node"
+import { validate } from "./validate"
+import { prisma } from "~/root"
+import { useActionData } from "@remix-run/react"
+
+export async function action({request}:ActionFunctionArgs){
+    const formData = await request.formData()
+    const email = String(formData.get("email"))
+    const password = String(formData.get("password"))
+    const name = String(formData.get("name"))
+
+    const errors = validate(email, name, password)
+
+    if (errors){
+        return json(errors)
+    } else if (!errors) {
+         
+    const createUser = (email?:string, name?:string, password?:string) => {
+        prisma.user.create({
+            data: {
+                email: email as string,
+                name: name as string,
+                password: password as string,
+            }
+        })
+    }
+}
+    return redirect("/")
+}
+
 export default function SignUp(){
+    const actionData = useActionData<typeof action>()
+    const emailError = actionData?.errors?.email
+    const nameError = actionData?.errors?.name 
+    const passwordError = actionData?.errors?.password 
+
     return (
         <>
         <div className="flex flex-col min-h-screen justify-center items-center">
             <h1 className="font-bold text-2xl">Sign Up</h1>
             <form method="POST">
                 <div>
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor="email">Email {" "} { }</label>
                     <div>
                     <input className="outline outline-1 rounded-sm" type="text" name="email" />
                     </div>
