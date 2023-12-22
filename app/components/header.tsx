d0ce85e2cf2a54d606168f242f769f30fce5ff16
julia <juliasoulid@gmail.com>
2023-12-22 10:17:21 -0300

    Header component created

---
+++ app/components/header.tsx
@@ -0,0 +1,19 @@
+export default function Header(){
+    return (
+        <>
+            <div className="bg-zinc-700 p-4 text-white flex justify-between">
+                <div>
+                <p>Logo</p>
+                </div>
+                <div className="flex gap-8">
+                    <p>GIT</p>
+                    <p>VIDEOS</p>
+                    <>RESOURCES</>
+                </div>
+                <div>
+                    <p>LOG IN</p>
+                </div>
+            </div>
+        </>
+    )
+}
